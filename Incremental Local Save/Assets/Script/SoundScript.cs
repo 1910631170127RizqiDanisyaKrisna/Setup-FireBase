685d2147ecd6f92de1a34e5e88f113a58a11bd80
Rizqi Danisya Krisna <83813922+1910631170127RizqiDanisyaKrisna@users.noreply.github.com>
2021-10-05 22:13:30 +0700

    Local Save/Load

---
+++ Incremental Local Save/Assets/Script/SoundScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    public static AudioClip Click, Upgrade, notUpgrade ;
+    static AudioSource audioSource;
+    void Start()
+    {
+        Click = Resources.Load<AudioClip>("CLICK");
+        Upgrade = Resources.Load<AudioClip>("Buying");
+        notUpgrade = Resources.Load<AudioClip>("BuyDenied");
+        
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+       switch (clip){
+        case "Click":
+        audioSource.PlayOneShot(Click);
+        break;
+       }
+
+       switch (clip){
+        case "UPGRADE":
+        audioSource.PlayOneShot(Upgrade);
+        break;
+       }
+
+       switch (clip){
+        case "notUpgrade":
+        audioSource.PlayOneShot(notUpgrade);
+        break;
+       }
+
+    }
+}
---
+++ Incremental Local Save/Assets/Script/Taparea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Taparea : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+    }
+}
